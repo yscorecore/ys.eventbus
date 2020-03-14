d59ab6e8c25b866ea1c2ab0a24109ca244b2c2fe
ypb <obpt@sina.com>
2020-03-14 17:11:40 +0800

    add test projects

---
+++ src/YS.EventBus.Impl.RabbitMQ/RabbitMQEventProducer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace YS.EventBus.Impl.RabbitMQ
+{
+
+    [ServiceClass]
+    public class RabbitMQEventProducer : IEventProducer
+    {
+        public Task Broadcast<T>(T enentData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish<T>(T eventData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/YS.EventBus.Core.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Knife.Hosting;
+namespace YS.EventBus.Core.UnitTest
+{
+    [TestClass]
+    public class UnitTest : KnifeHost
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
