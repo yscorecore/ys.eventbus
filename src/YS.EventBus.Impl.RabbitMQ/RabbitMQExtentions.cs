18640e6f90ea5fc6529fb19f509a464732392b42
ypb <obpt@sina.com>
2020-04-09 22:02:26 +0800

    add argument null check

--- src/YS.EventBus.Impl.RabbitMQ/RabbitMQExtentions.cs
+++ src/YS.EventBus.Impl.RabbitMQ/RabbitMQExtentions.cs
@@ -1,21 +1,25 @@
-﻿using RabbitMQ.Client;
+﻿using System;
+using RabbitMQ.Client;
 namespace YS.EventBus.Impl.RabbitMQ
 {
     public static class RabbitMQExtentions
     {
         public static void ExchangeDeclare(this IModel model, string exchange, EventType eventType)
         {
+            _ = model ?? throw new ArgumentNullException(nameof(model));
             var exchangeType = eventType == EventType.Topic ? ExchangeType.Fanout : ExchangeType.Direct;
             model.ExchangeDeclare(exchange, exchangeType, true, false, null);
         }
         public static string QueueDeclareAndBind(this IModel model, string exchange)
         {
+            _ = model ?? throw new ArgumentNullException(nameof(model));
             var queueInfo = model.QueueDeclare(exchange, true, false, false, null);
             model.QueueBind(queueInfo.QueueName, exchange, exchange, null);
             return queueInfo.QueueName;
         }
         public static string TemporaryQueueDeclareAndBind(this IModel model, string exchange)
         {
+            _ = model ?? throw new ArgumentNullException(nameof(model));
             var queueInfo = model.QueueDeclare("", true, false, true, null);
             model.QueueBind(queueInfo.QueueName, exchange, exchange, null);
             return queueInfo.QueueName;
